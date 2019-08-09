ae5c0a08327e9e863c50f57d51f5261df7e0e37c
地图项目部--Java--邓彬 <ahudengbin@126.com>
2019-08-09 17:56:51 +0800

    feat: 提交代码

---
+++ SecondsKillSystem/src/main/java/com/ahu/SecondsKillSystem/SecondsKillSystemApplication.java
@@ -0,0 +1,14 @@
+package com.ahu.SecondsKillSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SecondsKillSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondsKillSystemApplication.class, args);
+	}
+
+}

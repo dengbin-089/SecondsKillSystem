42fdd1a0c52f9097cdf7997d8162e8c111be4484
地图项目部--Java--邓彬 <ahudengbin@126.com>
2019-08-12 09:50:06 +0800

    feat: 提交代码

--- SecondsKillSystem/src/main/java/com/ahu/SecondsKillSystem/entity/Msmerchant.java
+++ SecondsKillSystem/src/main/java/com/ahu/SecondsKillSystem/entity/Msmerchant.java
@@ -1,16 +1,36 @@
 package com.ahu.SecondsKillSystem.entity;
 
 import lombok.Data;
-
 import java.io.Serializable;
 
+/**
+ * 商家信息表实体
+ */
 @Data
 public class Msmerchant implements Serializable {
 	private static final long serialVersionUID = 732298146760455085L;
+	/**
+	 * 商家表主键
+	 */
 	private int id;
+	/**
+	 * 商家姓名
+	 */
 	private String merchantName;
+	/**
+	 * 商家账号
+	 */
 	private String merchantAccount;
+	/**
+	 * 商家密码
+	 */
 	private String merchantPassword;
+	/**
+	 * 经营范围
+	 */
 	private String merchantScope;
+	/**
+	 * 商家店铺名称
+	 */
 	private String merchantShopName;
 }

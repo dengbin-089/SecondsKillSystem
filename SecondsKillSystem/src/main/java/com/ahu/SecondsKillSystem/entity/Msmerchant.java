d5275e66627d2493e164e5c456924cb90935a90d
地图项目部--Java--邓彬 <ahudengbin@126.com>
2019-08-12 09:46:13 +0800

    feat: 提交代码

--- SecondsKillSystem/src/main/java/com/ahu/SecondsKillSystem/entity/Msmerchant.java
+++ SecondsKillSystem/src/main/java/com/ahu/SecondsKillSystem/entity/Msmerchant.java
@@ -1,57 +1,16 @@
 package com.ahu.SecondsKillSystem.entity;
 
+import lombok.Data;
 
-public class Msmerchant {
-	
+import java.io.Serializable;
+
+@Data
+public class Msmerchant implements Serializable {
+	private static final long serialVersionUID = 732298146760455085L;
 	private int id;
-	private String merchantname;
-	private String merchantaccount;
-	private String merchantpassword;
-	private String merchantscope;
-	private String merchantshopname;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getMerchantname() {
-		return merchantname;
-	}
-	public void setMerchantname(String merchantname) {
-		this.merchantname = merchantname;
-	}
-	public String getMerchantaccount() {
-		return merchantaccount;
-	}
-	public void setMerchantaccount(String merchantaccount) {
-		this.merchantaccount = merchantaccount;
-	}
-	public String getMerchantpassword() {
-		return merchantpassword;
-	}
-	public void setMerchantpassword(String merchantpassword) {
-		this.merchantpassword = merchantpassword;
-	}
-	public String getMerchantscope() {
-		return merchantscope;
-	}
-	public void setMerchantscope(String merchantscope) {
-		this.merchantscope = merchantscope;
-	}
-	public String getMerchantshopname() {
-		return merchantshopname;
-	}
-	public void setMerchantshopname(String merchantshopname) {
-		this.merchantshopname = merchantshopname;
-	}
-	
-	@Override
-	public String toString() {
-		return "Msmerchant [id=" + id + ", merchantname=" + merchantname + ", merchantaccount=" + merchantaccount
-				+ ", merchantpassword=" + merchantpassword + ", merchantscope=" + merchantscope + ", merchantshopname="
-				+ merchantshopname + "]";
-	}
-	
-	
+	private String merchantName;
+	private String merchantAccount;
+	private String merchantPassword;
+	private String merchantScope;
+	private String merchantShopName;
 }
